f62d5b5896e5863656d6a88908f2c30ae0518283
Cullen Johnson <cullen.d.johnson@gmail.com>
2025-06-28 00:01:57 -0700

    added templates and created user model

--- quizout-server/app/main/auth.py
+++ quizout-server/app/main/auth.py
@@ -1,14 +1,14 @@
-from flask import Blueprint
+from flask import render_template
 from . import auth_blueprint as auth
 from .. import db
 
 @auth.route('/login')
 def login():
-    return 'Login'
+    return render_template('login.html')
 
 @auth.route('/signup')
 def signup():
-    return 'Signup'
+    return render_template('signup.html')
 
 @auth.route('/logout')
 def logout():
--- quizout-server/app/main/main.py
+++ quizout-server/app/main/main.py
@@ -6,3 +6,6 @@ from .. import db
 def index():
     return render_template('index.html')
 
+@main.route('/profile')
+def profile():
+    return render_template('profile.html')

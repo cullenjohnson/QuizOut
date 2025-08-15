45799f243a006dc43247a07559f38c13944f282b
cullenjohnson <cullen.d.johnson@gmail.com>
2025-08-15 13:56:49 -0700

    raspberry pi didn't like the oggs, maybe mp3?

--- quizout-client/resources/sounds/__init__.py
+++ quizout-client/resources/sounds/__init__.py
@@ -5,9 +5,9 @@ from utils.Enums import SoundEffect
 BASE_DIR = os.path.dirname(__file__)
 
 paths = {
-    SoundEffect.ActivateSound : os.path.abspath(os.path.join(BASE_DIR, "activate.611802__metalfortress__reminder2.ogg")),
-    SoundEffect.BuzzSound : os.path.abspath(os.path.join(BASE_DIR, "buzz.342749__rhodesmas__notification-01.ogg")),
-    SoundEffect.CorrectSound : os.path.abspath(os.path.join(BASE_DIR, "correct.611787__metalfortress__jingle_little_achievement2.ogg")),
-    SoundEffect.IncorrectSound : os.path.abspath(os.path.join(BASE_DIR, "incorrect.611798__metalfortress__error.ogg")),
-    SoundEffect.TimeoutSound : os.path.abspath(os.path.join(BASE_DIR, "timeout.745159__etheraudio__incorrect-buzzer-retro.ogg"))
+    SoundEffect.ActivateSound : os.path.abspath(os.path.join(BASE_DIR, "activate.611802__metalfortress__reminder2.mp3")),
+    SoundEffect.BuzzSound : os.path.abspath(os.path.join(BASE_DIR, "buzz.342749__rhodesmas__notification-01.mp3")),
+    SoundEffect.CorrectSound : os.path.abspath(os.path.join(BASE_DIR, "correct.611787__metalfortress__jingle_little_achievement2.mp3")),
+    SoundEffect.IncorrectSound : os.path.abspath(os.path.join(BASE_DIR, "incorrect.611798__metalfortress__error.mp3")),
+    SoundEffect.TimeoutSound : os.path.abspath(os.path.join(BASE_DIR, "timeout.745159__etheraudio__incorrect-buzzer-retro.mp3"))
 }
